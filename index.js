d100d674bf5f51e92e5f1d2ba7b714d246bf4505
Andrey Sholinka <sholinka2111@gmail.com>
2023-04-04 00:23:55 +0700

    pokemon

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const logger = require('morgan');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3030;
+
+const apiRouter = require('./src/routers');
+
+const app = express();
+const router = express.Router();
+
+app.use(cors())
+app.use(logger('dev'));
+
+app.use(express.json({
+    limit: '50mb'
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    parameterLimit: 100000,
+    limit: '50mb'
+}));
+
+const db = require("./src/models");
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "Welcome to Pokemon API X PokeAPI"
+    });
+});
+
+app.get("/close", (req, res) => {
+    res.send({
+        message: "Exiting NodeJS server"
+    });
+    process.exit()
+});
+
+app.get('/_health', (req, res) => {
+    res.status(200).send('ok')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+
+app.use('/api', router);
+
+router.use('/v1', apiRouter);
+
+app.use((req, res) => {
+    res.status(404).send('404 Page Not Found')
+});
