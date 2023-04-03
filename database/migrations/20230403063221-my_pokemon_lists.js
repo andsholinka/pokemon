164c08e3e8632b9ca7afad71523e34bd3694b6b4
Andrey Sholinka <sholinka2111@gmail.com>
2023-04-04 00:13:44 +0700

    migrations

---
+++ database/migrations/20230403063221-my_pokemon_lists.js
@@ -0,0 +1,37 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("My_Pokemon_Lists", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      pokemon_name: {
+        type: Sequelize.STRING,
+      },
+      nickname: {
+        type: Sequelize.STRING,
+      },
+      fibonacci: {
+        type: Sequelize.INTEGER,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable("My_Pokemon_Lists");
+  },
+};
